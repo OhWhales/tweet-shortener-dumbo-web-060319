1f0050778370add0463aca3080f262a232d205d2
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-31 01:22:53 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,6 +40,21 @@ def bulk_tweet_shortener(tweet)
   increment = 0 
   while (increment < tweet.length)
   
+    finalWord=str.split(" ")
+    key = dictionary.keys 
+    counter = 0 
+    while (counter < finalWord.length)
+      counter2=0 
+      while (counter2 < key.length)
+        if finalWord[counter] == key[counter2]
+          z=finalWord[counter]
+          finalWord[counter] = dictionary[z]
+        end 
+        counter2 = counter2 + 1
+      end 
+      counter = counter + 1
+    end 
+    puts finalWord.join(" ")
   
   increment = increment + 1 
   end
